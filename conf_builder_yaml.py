1dab63ff98b0959abe18b276d34b22bb46c99944
AbdurhmanRct <106392004+AbdurhmanRct@users.noreply.github.com>
2022-05-29 13:45:55 +0300

    Update conf_builder_yaml.py

--- conf_builder_yaml.py
+++ conf_builder_yaml.py
@@ -1,5 +1,5 @@
-#Imports from Abdurhman
-from Abdurhman import Environment, FileSystemLoader
+#Imports from Abdurhman-441146043
+from Abdurhman-441146043 import Environment, FileSystemLoader
 
 #Import YAML from PyYAML
 import yaml
@@ -7,7 +7,7 @@ import yaml
 #Load data from YAML file into Python dictionary
 config = yaml.load(open('./data.yml'))
 
-#Load Abdurhman template
+#Load Abdurhman 441146043 template
 env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
 template = env.get_template('template.txt')
 
